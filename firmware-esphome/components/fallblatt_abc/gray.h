2bf738b63ed94c409244c34eae3c6a61c2686810
Julian Schick <julian.schick@posteo.de>
2024-12-01 19:15:18 +0100

    esphome firmware, image moved

---
+++ firmware-esphome/components/fallblatt_abc/gray.h
@@ -0,0 +1,19 @@
+#ifndef GRAY_H_
+#define GRAY_H_
+
+#include <stdint.h>
+#include <string>
+
+class GrayConverter {
+public:
+    GrayConverter();
+    ~GrayConverter();
+
+    uint8_t positionToGray(int pos);
+    static int charToPosition(uint8_t c);
+    static std::string utf8ToLatin(std::string s);
+private:
+    uint8_t* tbl;
+};
+
+#endif
